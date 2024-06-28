be1fc2e103beeed470c219fea18327a316f0a627
KelioXenelly <kelioxenelly0507@gmail.com>
2024-06-28 16:03:24 +0700

    initial commit

--- produk.php
+++ produk.php
@@ -31,6 +31,7 @@
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img/icon-logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
@@ -84,7 +85,7 @@
                                             </div>
                                             <div class="card-body">
                                                 <h4 class="card-title fs-4">'.$produk['nama'].'</h4>
-                                                <h5 class="card-title fs-5">'.$produk['kadar'].'k / '.$produk['berat'].' gr</h5>
+                                                <h5 class="card-title fs-5">'.$produk['kadar'].'k / '.str_replace(".", ",", $produk['berat']).' gr</h5>
                                                 <p class="card-text text-truncate">'.$produk['detail'].'</p>
                                                 <p class="card-text fs-5 text-warning">Rp'.str_replace(',', '.', number_format($produk["harga"])).'</p>
                                 ';
